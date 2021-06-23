39a6c6ae2ba6d37d7981c551658f851c57240618
Willian Del Freu <williankeyex@gmail.com>
2021-06-22 23:22:14 -0300

    chore: add routes and link to pages

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
 
@@ -28,7 +28,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
