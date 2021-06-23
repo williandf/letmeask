e798296e18ac838f39c5cc4c184718249ef8ebb5
Willian Del Freu <williankeyex@gmail.com>
2021-06-23 00:39:33 -0300

    chore: create context and hook auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import { useContext } from "react";
 import { useHistory } from "react-router-dom";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -12,7 +11,7 @@ import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
-  const { signInWithGoogle, user } = useContext(AuthContext)
+  const { signInWithGoogle, user } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
