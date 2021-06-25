6c1d50622c473ad197478f84cbf9c42232b5616d
Willian Del Freu <williankeyex@gmail.com>
2021-06-25 12:37:25 -0300

    chore: structure questions

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent, useEffect } from "react";
 import { useParams } from "react-router-dom"
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
+import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
@@ -18,7 +19,7 @@ type FirebaseQuestions = Record<string, {
   isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string,
@@ -37,7 +38,7 @@ export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
   const roomId = params.id;
 
@@ -122,7 +123,17 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   )
